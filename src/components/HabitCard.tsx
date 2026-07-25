92f8d30e15bb40643eabd14c77822109dcb8a8af
scmlewis <scmlewis@gmail.com>
2026-07-25 15:58:31 +0800

    feat: pass habit color to ContributionGrid in HabitCard

--- src/components/HabitCard.tsx
+++ src/components/HabitCard.tsx
@@ -332,7 +332,7 @@ export function HabitCard({ habit, onArchived, onCheckIn, onTap, onDragStart, on
           </div>
         </div>
         <div className="mt-3">
-          <ContributionGrid logs={logs} cellSize={10} cellGap={2} showLabels={false} showLegend={false} />
+          <ContributionGrid logs={logs} cellSize={10} cellGap={2} showLabels={false} showLegend={false} color={habit.color} />
         </div>
       </div>
     </>
